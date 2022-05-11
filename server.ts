16cf15d1eeb359598b0cfaa5ec1d717cdabc8877
Tiffany Wilmot-Simpson <twilmotsimpson@gmail.com>
2022-05-11 16:28:04 +0100

    updated my packagejson and server file to run server on local

--- server.ts
+++ server.ts
@@ -27,8 +27,8 @@ const client = new Client(dbConfig);
 client.connect();
 
 app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
-  res.json(dbres.rows);
+  res.send("Hello World");
+  
 });
 
 

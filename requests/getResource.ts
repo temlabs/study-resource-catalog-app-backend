10ff3419d4f5110dc61e1b70cefbe7f930796ff9
Tiffany Wilmot-Simpson <twilmotsimpson@gmail.com>
2022-05-11 14:42:17 +0100

    added error handling for get resources

--- requests/getResource.ts
+++ requests/getResource.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import { Client } from "pg";
 
+const herokuSSLSetting = { rejectUnauthorized: false }
+const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
+const dbConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: sslSetting,
+};
+
 const app = express();
 const client = new Client(dbConfig);
 
 
 app.get("/resources", async (req, res) => {
+    try{
     const query = "SELECT * FROM resource ORDER BY post_date  DESC;"
     const dbres = await client.query(query);
     res.json(dbres.rows);
+    res.status(200)
+    }catch(error){
+        res.status(400).send(error.stack)
+    }
   });
